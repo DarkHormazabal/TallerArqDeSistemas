94bc16042d23d1dd7782ed6dcaa085739f3e99fb
MatiassOrellana <matias.orellana@alumnos.ucn.cl>
2024-05-01 04:57:20 -0400

    fix: added atribute deleted and added lombok's dependency

---
+++ src/main/java/org/example/Models/BaseModel.java
@@ -0,0 +1,15 @@
+package org.example.Models;
+import io.ebean.annotation.WhenCreated;
+import io.ebean.annotation.WhenModified;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+public class BaseModel {
+
+    private
+
+}
