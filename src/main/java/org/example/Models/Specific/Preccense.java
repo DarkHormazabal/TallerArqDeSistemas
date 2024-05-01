addfd48773599866cb3596142c1e04237daeb488
MatiassOrellana <matias.orellana@alumnos.ucn.cl>
2024-05-01 06:50:50 -0400

    feat: added Preccense.java

---
+++ src/main/java/org/example/Models/Specific/Preccense.java
@@ -0,0 +1,30 @@
+package org.example.Models.Specific;
+
+import io.ebean.annotation.NotNull;
+import lombok.*;
+import org.example.Models.BaseModel;
+
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Preccense Class.
+ *
+ * @author Matias Orellana Hormazábal.
+ */
+@ToString
+@MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@Entity
+public class Preccense extends BaseModel {
+
+    /**
+     * The color.
+     */
+    @NotNull
+    private String color;
+
+}
